f2eed80de7b0e23f9e055fc59060dce5e4389bf3
BdVade <victoraderibigbe03@gmail.com>
2021-08-21 02:13:24 +0100

    Heroku stuff

--- HNGi8Stage2/settings.py
+++ HNGi8Stage2/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'victoraderibigbe04@gmail.com'
-EMAIL_HOST_PASSWORD = 'Adegbemiga'
+EMAIL_HOST_PASSWORD = os.environ.get("mail_password")
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'victoraderibigbe04@gmail.com'
